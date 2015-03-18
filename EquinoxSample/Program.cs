e9a5345a19b67d087704ecc069dc74fd66baca25
moostafaa <mostafavakily@gmail.com>
2015-03-18 14:42:31 +0330

    no error by 2015-18-03

---
+++ EquinoxSample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using Equinox;
+
+namespace EquinoxSample
+{
+    class Program
+    {
+        public static string ToShamsiDateTime(DateTime gregorianDateTime)
+        {
+            var year = gregorianDateTime.Year;
+            var month = gregorianDateTime.Month;
+            var day = gregorianDateTime.Day;
+            var persianCalendar = new PersianCalendar();
+            var yearShamsi = persianCalendar.GetYear(new DateTime(year, month, day, new GregorianCalendar()));
+            var monthShamsi = persianCalendar.GetMonth(new DateTime(year, month, day, new GregorianCalendar()));
+            var dayShamsi = persianCalendar.GetDayOfMonth(new DateTime(year, month, day, new GregorianCalendar()));
+            return string.Format("{0}{1}{2}{1}{3} {4}:{5}:{6}",
+                yearShamsi,
+                "/",
+                monthShamsi.ToString("00", CultureInfo.InvariantCulture),
+                dayShamsi.ToString("00", CultureInfo.InvariantCulture),
+                gregorianDateTime.Hour.ToString("00"),
+                gregorianDateTime.Minute.ToString("00"),
+                gregorianDateTime.Second.ToString("00"));
+        }
+
+
+        static void Main(string[] args)
+        {
+            //this sample will compute persian new year start time
+            var gregorianDateTime = 2015.GetSunEventUtc(SunEvent.VernalEquinox).AddHours(3.5);
+            var persianDateTime = ToShamsiDateTime(gregorianDateTime);
+            Console.WriteLine("{0} -> {1}", 2015, persianDateTime);
+        }
+    }
+}
